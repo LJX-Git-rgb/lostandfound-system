ff7f1cf9a7b17ec0ecf1900b78c6494ba9d8891f
jason <jason@dynadot.com>
2022-04-05 14:49:22 +0800

    some setting

--- lostandfound-web/vue.config.js
+++ lostandfound-web/vue.config.js
@@ -11,6 +11,10 @@ module.exports = {
                 target: 'http://localhost:8082',// 要跨域的域名
                 changeOrigin: true, // 是否开启跨域
             },
+            '/account': {
+                target: 'http://localhost:8082',// 要跨域的域名
+                changeOrigin: true, // 是否开启跨域
+            },
         }
     }
 }
