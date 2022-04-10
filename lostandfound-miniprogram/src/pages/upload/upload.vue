baca077bdc32fdd8050dfcbcc9908a5c9ec3e866
jason <jason@dynadot.com>
2022-04-10 20:21:14 +0800

    miniprogram --add change upload method

--- lostandfound-miniprogram/src/pages/upload/upload.vue
+++ lostandfound-miniprogram/src/pages/upload/upload.vue
@@ -1,205 +1,89 @@
 <template>
-	<view class="upload-form">
-		<!-- 基础用法，不包含校验规则 -->
-		<uni-forms ref="baseForm" :modelValue="baseFormData">
-			<uni-forms-item label="标题" required>
-				<uni-easyinput v-model="baseFormData.name" placeholder="请起个标题吧" />
-			</uni-forms-item>
-			<uni-forms-item label="描述" required>
-				<uni-easyinput v-model="baseFormData.age" placeholder="请输入描述,我们将根据他匹配结果" />
-			</uni-forms-item>
-			<uni-forms-item label="丢失类别" required>
-				<uni-data-checkbox v-model="baseFormData.hobby" multiple :localdata="hobbys" />
-			</uni-forms-item>
-			<uni-forms-item label="日期时间">
-				<uni-datetime-picker type="datetime" return-type="timestamp" v-model="baseFormData.datetimesingle"/>
-			</uni-forms-item>
-			<uni-forms-item>
-				<uni-file-picker limit="9" title="最多选择9张图片"></uni-file-picker>
-			</uni-forms-item>
-		</uni-forms>
+	<view class="body">
+		<view class="uni-padding-wrap uni-common-mt">
+			<uni-segmented-control 
+				:current="current" 
+				:values="items" 
+				style-type="text"
+				@clickItem="onClickItem" />
+		</view>
+		<view class="upload-form">
+			<view v-if="current === 0">
+				<uni-forms ref="baseForm" :modelValue="uploadForm">
+					<uni-forms-item label="标题" required>
+						<uni-easyinput v-model="uploadForm.name" placeholder="请起个标题吧"/>
+					</uni-forms-item>
+					<uni-forms-item label="描述" required>
+						<uni-easyinput v-model="uploadForm.age" placeholder="请输入描述,我们将根据他匹配结果" />
+					</uni-forms-item>
+					<uni-forms-item label="丢失类别" required>
+						<uni-data-checkbox v-model="uploadForm.hobby" multiple :localdata="hobbys" />
+					</uni-forms-item>
+					<uni-forms-item label="日期时间">
+						<uni-datetime-picker type="datetime" return-type="timestamp" v-model="uploadForm.datetimesingle"/>
+					</uni-forms-item>
+					<uni-forms-item>
+						<uni-file-picker limit="9" title="最多选择9张图片"></uni-file-picker>
+					</uni-forms-item>
+					<uni-forms-item>
+						<button @click="submit">Submit</button>
+					</uni-forms-item>
+
+				</uni-forms>
+			</view>
+
+			<view v-if="current === 1">
+				<uni-forms ref="baseForm" :modelValue="uploadForm">
+					<uni-forms-item label="标题" required>
+						<uni-easyinput v-model="uploadForm.name" placeholder="请起个标题吧" />
+					</uni-forms-item>
+					<uni-forms-item label="描述" required>
+						<uni-easyinput v-model="uploadForm.age" placeholder="请输入描述,我们将根据他匹配结果" />
+					</uni-forms-item>
+					<uni-forms-item label="丢失类别" required>
+						<uni-data-checkbox v-model="uploadForm.hobby" multiple :localdata="hobbys" />
+					</uni-forms-item>
+					<uni-forms-item label="日期时间">
+						<uni-datetime-picker type="datetime" return-type="timestamp" v-model="uploadForm.datetimesingle"/>
+					</uni-forms-item>
+					<uni-forms-item>
+						<uni-file-picker limit="9" title="最多选择9张图片"></uni-file-picker>
+					</uni-forms-item>
+				</uni-forms>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default {
+export default {
 		data() {
 			return {
-				// 基础表单数据
-				baseFormData: {
-					name: '',
-					age: '',
-					introduction: '',
-					sex: 2,
-					hobby: [5],
-					datetimesingle: 1627529992399
-				},
-				// 表单数据
-				alignmentFormData: {
-					name: '',
-					age: '',
-				},
-				// 单选数据源
-				sexs: [{
-					text: '男',
-					value: 0
-				}, {
-					text: '女',
-					value: 1
-				}, {
-					text: '保密',
-					value: 2
-				}],
-				// 多选数据源
-				hobbys: [{
-					text: '跑步',
-					value: 0
-				}, {
-					text: '游泳',
-					value: 1
-				}, {
-					text: '绘画',
-					value: 2
-				}, {
-					text: '足球',
-					value: 3
-				}, {
-					text: '篮球',
-					value: 4
-				}, {
-					text: '其他',
-					value: 5
-				}],
-				// 分段器数据
+				uploadForm: {},
+				hobbys:["卡","包","生活用品","现金"],
+				items: ["失物招领","寻物启事"],
 				current: 0,
-				items: ['左对齐', '顶部对齐'],
-				// 校验表单数据
-				valiFormData: {
-					name: '',
-					age: '',
-					introduction: '',
-				},
-				// 校验规则
-				rules: {
-					name: {
-						rules: [{
-							required: true,
-							errorMessage: '姓名不能为空'
-						}]
-					},
-					age: {
-						rules: [{
-							required: true,
-							errorMessage: '年龄不能为空'
-						}, {
-							format: 'number',
-							errorMessage: '年龄只能输入数字'
-						}]
-					}
-				},
-				// 自定义表单数据
-				customFormData: {
-					name: '',
-					age: '',
-					hobby: []
-				},
-				// 自定义表单校验规则
-				customRules: {
-					name: {
-						rules: [{
-							required: true,
-							errorMessage: '姓名不能为空'
-						}]
-					},
-					age: {
-						rules: [{
-							required: true,
-							errorMessage: '年龄不能为空'
-						}]
-					},
-					hobby: {
-						rules: [{
-								format: 'array'
-							},
-							{
-								validateFunction: function(rule, value, data, callback) {
-									if (value.length < 2) {
-										callback('请至少勾选两个兴趣爱好')
-									}
-									return true
-								}
-							}
-						]
-					}
-
-				},
-				dynamicFormData: {
-					email: '',
-					domains: {}
-				},
-				dynamicLists: [],
-				dynamicRules: {
-					email: {
-						rules: [{
-							required: true,
-							errorMessage: '域名不能为空'
-						}, {
-							format: 'email',
-							errorMessage: '域名格式错误'
-						}]
-					}
-				}
 			}
-		},
-		computed: {
-			// 处理表单排列切换
-			alignment() {
-				if (this.current === 0) return 'left'
-				if (this.current === 1) return 'top'
-				return 'left'
-			}
-		},
-		onLoad() {},
-		onReady() {
-			// 设置自定义表单校验规则，必须在节点渲染完毕后执行
-			this.$refs.customForm.setRules(this.customRules)
 		},
 		methods: {
 			onClickItem(e) {
-				console.log(e);
-				this.current = e.currentIndex
-			},
-			add() {
-				this.dynamicLists.push({
-					label: '域名',
-					rules: [{
-						'required': true,
-						errorMessage: '域名项必填'
-					}],
-					id: Date.now()
-				})
-			},
-			del(id) {
-				let index = this.dynamicLists.findIndex(v => v.id === id)
-				this.dynamicLists.splice(index, 1)
-			},
-			submit(ref) {
-				this.$refs[ref].validate().then(res => {
-					console.log('success', res);
-					uni.showToast({
-						title: `校验通过`
-					})
-				}).catch(err => {
-					console.log('err', err);
-				})
+				if (this.current !== e.currentIndex) {
+					this.current = e.currentIndex
+				}
 			},
 		}
-	}
+}
 </script>
 
-<style scoped>
-    /* @import '../../..//miniprogram_npm/weui-miniprogram/weui-wxss/dist/style/weui.wxss'; */
-	.upload-form{
-		margin: 50rpx 60rpx;
+<style lang="scss" scoped>
+	.body{
+		.uni-common-mt {
+			margin: 10rpx;
+			padding: 0px 50rpx;
+		}
+		.upload-form{
+			padding: 70rpx;
+		}
 	}
+	
 </style>
