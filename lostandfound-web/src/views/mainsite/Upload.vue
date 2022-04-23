3daeac55c24c35bc679743541f6b60a4255904e2
jason <jason@dynadot.com>
2022-04-23 18:23:03 +0800

    fixed upload (can upload more than two imgs

--- lostandfound-web/src/views/mainsite/Upload.vue
+++ lostandfound-web/src/views/mainsite/Upload.vue
@@ -1,62 +1,62 @@
 <template>
-    <!--    lost就是失物招领，find就是寻物启事-->
+    <!--    find就是失物招领，lost就是寻物启事-->
     <div id="body">
         <div id="radio">
             <el-radio v-model="lostOrFind" :label=true>失物招领</el-radio>
             <el-radio v-model="lostOrFind" :label=false>寻物启事</el-radio>
         </div>
         <hr>
-        <div id="findUpload" v-if="!lostOrFind">
-            <span>寻物启事 : </span>
-            <el-form ref="form" :model="form" label-width="120px">
-                <el-form-item label="标题：">
-                    <el-input v-model="form.title"></el-input>
-                </el-form-item>
+<!--        <div id="findUpload" v-if="!lostOrFind">-->
+<!--            <span>寻物启事 : </span>-->
+<!--            <el-form ref="form" :model="form" label-width="120px">-->
+<!--                <el-form-item label="标题：">-->
+<!--                    <el-input v-model="form.title"></el-input>-->
+<!--                </el-form-item>-->
 
-                <el-form-item label="丢失区域：">
-                    <el-input v-model="form.area"></el-input>
-                </el-form-item>
+<!--                <el-form-item label="丢失区域：">-->
+<!--                    <el-input v-model="form.area"></el-input>-->
+<!--                </el-form-item>-->
 
-                <el-form-item label="丢失时间：">
-                    <el-date-picker
-                        v-model="form.date"
-                        type="date"
-                        placeholder="选择日期：">
-                    </el-date-picker>
-                </el-form-item>
+<!--                <el-form-item label="丢失时间：">-->
+<!--                    <el-date-picker-->
+<!--                        v-model="form.date"-->
+<!--                        type="date"-->
+<!--                        placeholder="选择日期：">-->
+<!--                    </el-date-picker>-->
+<!--                </el-form-item>-->
 
-                <el-form-item label="失物类别：">
-                    <el-checkbox-group v-model="form.selectedType">
-                        <el-checkbox v-for="item in type" :label="item" :key="item">{{ item }}</el-checkbox>
-                    </el-checkbox-group>
-                </el-form-item>
+<!--                <el-form-item label="失物类别：">-->
+<!--                    <el-checkbox-group v-model="form.selectedType">-->
+<!--                        <el-checkbox v-for="item in type" :label="item" :key="item">{{ item }}</el-checkbox>-->
+<!--                    </el-checkbox-group>-->
+<!--                </el-form-item>-->
 
-                <el-form-item label="失物详情描述：">
-                    <el-input type="textarea" v-model="form.desc"></el-input>
-                </el-form-item>
+<!--                <el-form-item label="失物详情描述：">-->
+<!--                    <el-input type="textarea" v-model="form.desc"></el-input>-->
+<!--                </el-form-item>-->
 
-                <el-form-item>
-                    <el-upload
-                        class="upload-demo"
-                        ref="upload"
-                        drag
-                        action="http://localhost:8081/losts/addImg"
-                        multiple
-                        :on-preview="handlePreview"
-                        :on-remove="handleRemove"
-                        :on-success="imgSuccess"
-                        :auto-upload="false">
-                        <i class="el-icon-upload"></i>
-                        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-                        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
-                    </el-upload>
-                </el-form-item>
+<!--                <el-form-item>-->
+<!--                    <el-upload-->
+<!--                        class="upload-demo"-->
+<!--                        ref="upload"-->
+<!--                        drag-->
+<!--                        action="http://localhost:8081/losts/addImg"-->
+<!--                        multiple-->
+<!--                        :on-preview="handlePreview"-->
+<!--                        :on-remove="handleRemove"-->
+<!--                        :on-success="imgSuccess"-->
+<!--                        :auto-upload="false">-->
+<!--                        <i class="el-icon-upload"></i>-->
+<!--                        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>-->
+<!--                        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>-->
+<!--                    </el-upload>-->
+<!--                </el-form-item>-->
 
-                <el-form-item>
-                    <el-button type="primary" @click="submitUpload">立即发布</el-button>
-                </el-form-item>
-            </el-form>
-        </div>
+<!--                <el-form-item>-->
+<!--                    <el-button type="primary" @click="submitUpload">立即发布</el-button>-->
+<!--                </el-form-item>-->
+<!--            </el-form>-->
+<!--        </div>-->
 
         <div id="lostUpload" v-if="lostOrFind">
             <span>失物招领 : </span>
@@ -89,14 +89,16 @@
 
                 <el-form-item>
                     <el-upload
+                        multiple
                         class="upload-demo"
                         ref="upload"
                         drag
-                        action="http://localhost:8081/finds/addImg"
-                        multiple
+                        action
                         :on-preview="handlePreview"
+                        :on-change="handleChange"
                         :on-remove="handleRemove"
                         :on-success="imgSuccess"
+                        :file-list="fileList"
                         :auto-upload="false">
                         <i class="el-icon-upload"></i>
                         <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
@@ -118,10 +120,16 @@ export default {
     name: "upload",
     data() {
         return {
+            //判断是lost or found
             lostOrFind: false,
+
             //类别
             type: ["食品", "电子产品", "生活用品", "手机", "卡", "现金"],
 
+            //上传的照片集合
+            fileList: [],
+
+            //传递的信息
             form: {
                 title: "",
                 desc: '',
@@ -135,67 +143,87 @@ export default {
     methods: {
         //发布按钮的点击事件,上传图片
         submitUpload() {
-            this.$refs.upload.submit();
+            //判断是否有文件再上传
+            if (this.fileList.length === 0) {
+                //没有的话直接上传信息，不上传图片
+            }else {
+                // 下面的代码将创建一个空的FormData对象:
+                var formData = new FormData()
+                // 你可以使用FormData.append来添加键/值对到表单里面；
+                this.fileList.forEach((file) => {
+                    formData.append('file', file.raw)
+                })
+
+                this.$axios
+                    .post("/api/losts/addImg", formData)
+                    //判断上传是否成功，成功返回一个img的地址然后请求后台添加其他数据
+                    .then(res=>{
+                        if (res.status == "200") {
+                            this.imgSuccess(res);
+                        }
+                    })
+            }
         },
         //上传成功事件
-        imgSuccess(res, file, fileList) {
-            //判断上传是否成功，成功返回一个img的地址然后请求后台添加其他数据
-            if (res.code == "200" || res.msg == "success") {
-                let tag = "", image = "";
-                for (let i = 0; i < this.form.selectedType.length; i++) {
-                    tag += this.form.selectedType[i] + '&';
-                }
+        imgSuccess(res) {
+            let tag = "", image = "";
+            for (let i = 0; i < this.form.selectedType.length; i++) {
+                tag += this.form.selectedType[i] + '&';
+            }
+            if (res.data.length > 0) {
                 for (let i = 0; i < res.data.length; i++) {
                     image += res.data[i] + '&';
                 }
-                if (!this.lostOrFind){
-                    this.$axios({
-                        method: "post",
-                        url:"/api/losts/add",
-                        data: {
-                            uid: this.$store.state.user.id,
-                            title: this.form.title,
-                            description: this.form.desc,
-                            lostArea: this.form.area + '&' + this.$store.state.user.location,
-                            lostTime: this.form.date,
-                            tag: tag,
-                            image: image
-                        }
-                    }).then(res => {
-                        this.form={};
-                        this.$router.push({path:'/'})
-                    }).catch(err => {
-                        this.$message.error("服务器错误，稍后再发布吧")
-                    })
-                }else{
-                    this.$axios({
-                        method: "post",
-                        url:"/api/finds/add",
-                        data: {
-                            uid: this.$store.state.user.id,
-                            title: this.form.title,
-                            description: this.form.desc,
-                            foundArea: this.form.area + '&' + this.$store.state.user.location,
-                            foundTime: this.form.date,
-                            tag: tag,
-                            image: image
-                        }
-                    }).then(res => {
-                        this.form={};
-                        this.$router.push({path:'/'})
-                    }).catch(err => {
-                        this.$message.error("服务器错误，稍后再发布吧")
-                    })
-                }
+            }
 
+            console.log("hahaha",this.form)
+            if (!this.lostOrFind) {
+                this.$axios({
+                    method: "post",
+                    url: "/api/losts/add",
+                    data: {
+                        uid: this.$store.state.user.id,
+                        title: this.form.title,
+                        description: this.form.desc,
+                        lostArea: this.form.area + '&' + this.$store.state.user.location,
+                        lostTime: this.form.date,
+                        tag: tag,
+                        image: image
+                    }
+                }).then(res => {
+                    this.form = {};
+                    this.$router.push({path: '/'})
+                }).catch(err => {
+                    this.$message.error("服务器错误，稍后再发布吧")
+                })
             } else {
-                this.$message.error(res.data.msg);
+                this.$axios({
+                    method: "post",
+                    url: "/api/finds/add",
+                    data: {
+                        uid: this.$store.state.user.id,
+                        title: this.form.title,
+                        description: this.form.desc,
+                        foundArea: this.form.area + '&' + this.$store.state.user.location,
+                        foundTime: this.form.date,
+                        tag: tag,
+                        image: image
+                    }
+                }).then(res => {
+                    this.form = {};
+                    this.$router.push({path: '/'})
+                }).catch(err => {
+                    this.$message.error("服务器错误，稍后再发布吧")
+                })
             }
         },
 
-        //上传组件的一些其他事件
+        //上传组件的一些钩子
         handleRemove(file, fileList) {
-            console.log(file, fileList);
+            this.fileList = fileList
+        },
+        handleChange(file, fileList) {
+            this.fileList = fileList
         },
         handlePreview(file) {
             console.log(file);
