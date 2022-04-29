9e241e3dd016062256b17fb566752df6ba6ba282
jason <jason@dynadot.com>
2022-04-29 18:10:27 +0800

    add MyUpload at minipro

--- lostandfound-miniprogram/src/pages/myinfo/MyUpload.vue
+++ lostandfound-miniprogram/src/pages/myinfo/MyUpload.vue
@@ -1,7 +1,50 @@
 <template>
-    
+    <view id="content">
+        <uni-search-bar
+            v-model="searchValue"
+            placeholder="搜索"
+            bgColor="#EEEEEE"
+            class="search-bar"
+            @confirm="search"
+            @blur="blur"
+            @focus="focus"
+            @input="input"
+            @cancel="cancel"
+            @clear="clear">
+        </uni-search-bar>
+        <uni-card :title="item.title" :sub-title="'发布时间' + item.createTime" class="card" v-for="item in goodsList"
+                  :key="item.id">
+            <!-- 轮播图 -->
+            <view @click="toGoodsInfo(item.id)">
+                <swiper class="swiper" :indicator-dots="true" :autoplay="true" :interval="2000" :duration="500">
+                    <swiper-item v-for="(url, index) in item.imageList" :key="index">
+                        <image :src="'../../../../image'  + url " alt="">
+                    </swiper-item>
+                </swiper>
+                <!-- desc -->
+                <text class="uni-body uni-mt-5">
+                    {{ item.description }}
+                </text>
+            </view>
+            <!-- 选项 -->
+            <view slot="actions" class="card-actions">
+                <button open-type="share" @click="shareId=item.id"></button>
+                <view class="card-actions-item" @click="actionsClick('分享')">
+                    <uni-icons type="staff" size="18" color="#999"></uni-icons>
+                    <text class="card-actions-item-text">分享</text>
+                </view>
+                <view class="card-actions-item" @click="actionsClick('点赞')">
+                    <uni-icons type="heart" size="18" color="#999"></uni-icons>
+                    <text class="card-actions-item-text">点赞</text>
+                </view>
+                <view class="card-actions-item" @click="actionsClick('评论')">
+                    <uni-icons type="chatbubble" size="18" color="#999"></uni-icons>
+                    <text class="card-actions-item-text">评论</text>
+                </view>
+            </view>
+        </uni-card>
+    </view>
 </template>
-
 <script>
 export default {
     data() {
@@ -54,6 +97,43 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.card {
+    swiper {
+        image {
+            width: 100%;
+            height: 100%;
+        }
+    }
 
+    text {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical
+    }
+
+    .card-actions {
+        display: flex;
+        border-top: 1px solid #EBEEF5;
+        padding: 20rpx;
+
+        button {
+            position: relative;
+            left: 120rpx;
+            opacity: 0;
+        }
+
+        .card-actions-item {
+            flex: 1;
+            text-align: center;
+
+            text {
+                margin-left: 8rpx;
+                color: rgb(106, 106, 106);
+            }
+        }
+    }
+}
 </style>
