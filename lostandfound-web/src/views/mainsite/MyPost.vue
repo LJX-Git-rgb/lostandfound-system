91c02d2fe7f05b9bbd3861b89c472c63c7fa095b
jason <jason@dynadot.com>
2022-05-05 22:12:24 +0800

    fixed delete mypost

--- lostandfound-web/src/views/mainsite/MyPost.vue
+++ lostandfound-web/src/views/mainsite/MyPost.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="timeBlock">
-        <span v-if="lostOrFind">我发布的寻物启事 :
+        <div style="display: flex;justify-content: space-between;width: 1000px;">
+            <span v-if="lostOrFind">我发布的寻物启事 :</span>
+            <span v-if="!lostOrFind">我发布的失物招领 :</span>
             <el-popover
                 placement="top"
                 width="160"
@@ -10,26 +12,14 @@
                     <el-button size="mini" type="text" @click="visible = false">取消</el-button>
                     <el-button type="primary" size="mini" @click="deleteAll">确定</el-button>
                 </div>
-                <el-button slot="reference">删除</el-button>
+                <el-button slot="reference" icon="el-icon-delete">全部清空</el-button>
             </el-popover>
-<!--            <el-button icon="el-icon-delete" @click="deleteAll"></el-button>-->
-        </span>
-        <span v-if="!lostOrFind">我发布的失物招领 :
-            <el-popover
-                placement="top"
-                width="160"
-                v-model="visible">
-                <p>确定要删除所有发布记录吗？</p>
-                <div style="text-align: right; margin: 0">
-                    <el-button size="mini" type="text" @click="visible = false">取消</el-button>
-                    <el-button type="primary" size="mini" @click="deleteAll">确定</el-button>
-                </div>
-                <el-button slot="reference">删除</el-button>
-            </el-popover>
-<!--            <el-button icon="el-icon-delete" @click="deleteAll"></el-button>-->
-        </span>
+        </div>
         <el-timeline>
             <el-timeline-item v-for="item in tableData" :key="item.id" :timestamp="new Date(item.createTime).format('yyyy-MM-dd hh:mm:ss')" placement="top">
+                <div class="d2">
+                    <el-button class="btn1" type="danger" icon="el-icon-delete" circle @click="deleteById(item.id)"></el-button>
+                </div>
                 <a @click="$router.push({path: 'goodsInfo', query:{state:$route.fullPath.split('?')[1], id:item.id}})">
                     <el-card >
                         <img :src="require('../../../../lostandfound-miniprogram/src/static/image' + item.imageList[0])" alt="" id="cardImg">
@@ -41,9 +31,6 @@
                         </div>
                     </el-card>
                 </a>
-                <div class="d2">
-                    <el-button class="btn1" type="danger" icon="el-icon-delete" circle @click="deleteById(item.id)"></el-button>
-                </div>
             </el-timeline-item>
         </el-timeline>
         <a id="archor">
@@ -82,10 +69,10 @@ export default {
               params:{
                   uid:this.$store.state.user.id,
                   flag: this.lostOrFind,
-                  visible:this.visible = true,
               }
           }).then(res =>{
-              console.log(res)
+              this.load();
+              this.visible = false;
           })
       },
       deleteById(id){
@@ -98,29 +85,32 @@ export default {
                   visible:this.visible = true,
               }
           }).then(res =>{
-              console.log(res)
+              this.load();
           })
-      }
+      },
+        load(){
+            this.$axios({
+                method:"get",
+                url:"/api/finds/byUser",
+                params:{
+                    uid:this.$store.state.user.id
+                }
+            }).then(res => {
+                this.findData = res.data;
+            })
+            this.$axios({
+                method:"get",
+                url:"/api/losts/byUser",
+                params:{
+                    uid:this.$store.state.user.id
+                }
+            }).then(res => {
+                this.lostData = res.data;
+            })
+        }
     },
     mounted() {
-        this.$axios({
-            method:"get",
-            url:"/api/finds/byUser",
-            params:{
-                uid:this.$store.state.user.id
-            }
-        }).then(res => {
-            this.findData = res.data;
-        })
-        this.$axios({
-            method:"get",
-            url:"/api/losts/byUser",
-            params:{
-                uid:this.$store.state.user.id
-            }
-        }).then(res => {
-            this.lostData = res.data;
-        })
+        this.load();
     }
 }
 </script>
@@ -152,12 +142,13 @@ export default {
         position: relative;
         display:flex;
         justify-content:flex-end;
-
     }
     .btn1 {
+        border: none;
+        border-radius: 0;
+        border-top-right-radius: 4px;
         position: relative;
-        right: 10px;
-        top: 10px;
+        top: 41px;
     }
     #cardImg {
         width: 20%;
