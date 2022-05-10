ed5ee1a36e825a3127a4737fd23f20b217820be8
Jennifer <yyqx1128wmt1202@dingtalk.com>
2022-05-10 23:26:24 +0800

    modifying item information(the information cannot be written to the database yet)

--- lostandfound-web/src/views/mainsite/MyPost.vue
+++ lostandfound-web/src/views/mainsite/MyPost.vue
@@ -18,6 +18,7 @@
         <el-timeline>
             <el-timeline-item v-for="item in tableData" :key="item.id" :timestamp="new Date(item.createTime).format('yyyy-MM-dd hh:mm:ss')" placement="top">
                 <div class="d2">
+                    <el-button class="btn2" type="primary" size="small" icon="el-icon-edit" circle @click="editUserContact">修改物品信息</el-button>
                     <el-button class="btn1" type="danger" icon="el-icon-delete" circle @click="deleteById(item.id)"></el-button>
                 </div>
                 <a @click="$router.push({path: 'goodsInfo', query:{state:$route.fullPath.split('?')[1], id:item.id}})">
@@ -36,6 +37,55 @@
         <a id="archor">
             广告位招租
         </a>
+<!--        修改物品信息弹出框-->
+        <el-dialog :visible.sync="ContactDialogVisible" title="修改" width="60%">
+            <el-form label-width="120px">
+                <el-form-item label="标题：">
+                    <el-input v-model="goods.title"></el-input>
+                </el-form-item>
+                <el-form-item :label="lostOrFind ? '发现区域：' : '丢失区域：'">
+                    <el-input v-model="goods.area"></el-input>
+                </el-form-item>
+                <el-form-item :label="lostOrFind ? '发现时间：' : '丢失时间：'">
+                    <el-date-picker
+                        v-model="goods.date"
+                        type="date"
+                        placeholder="选择日期：">
+                    </el-date-picker>
+                </el-form-item>
+                <el-form-item label="物品类别：">
+                    <el-checkbox-group v-model="goods.selectedType">
+                        <el-checkbox v-for="item in type" :label="item" :key="item">{{ item }}</el-checkbox>
+                    </el-checkbox-group>
+                </el-form-item>
+                <el-form-item label="物品详情描述：">
+                    <el-input type="textarea" v-model="goods.desc"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-upload
+                        multiple
+                        class="upload-demo"
+                        ref="upload"
+                        drag
+                        action
+                        :on-preview="handlePreview"
+                        :on-change="handleChange"
+                        :on-remove="handleRemove"
+                        :file-list="fileList"
+                        :auto-upload="false">
+                        <i class="el-icon-upload"></i>
+                        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+                        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+                    </el-upload>
+                </el-form-item>
+                <el-form-item>
+                      <span class="dialog-footer">
+                        <el-button @click="ContactDialogVisible = false">取消</el-button>
+                        <el-button type="primary" @click="ContactSave">确定</el-button>
+                      </span>
+                </el-form-item>
+            </el-form>
+        </el-dialog>
     </div>
 </template>
 
@@ -44,9 +94,22 @@ export default {
     name: "MyPost",
     data() {
         return {
+            lostOrFind: true,
+            type: ["食品", "电子产品", "生活用品", "手机", "卡", "现金"],
+            fileList: [],
             findData: [],
             lostData: [],
             visible: false,
+            goodsInfo:{},
+            goods:{
+                title: "",
+                desc: '',
+                area: '',
+                date: '',
+                selectedType: [],
+                imgList: '',
+            },
+            ContactDialogVisible:false,
         }
     },
     computed: {
@@ -106,8 +169,105 @@ export default {
             }).then(res => {
                 this.lostData = res.data;
             })
-        }
+        },
+        editUserContact(){
+            this.ContactDialogVisible=true
+            this.goods={}
+        },
+
+        ContactSave(){
+            if (this.fileList.length === 0) {
+                //没有的话直接上传信息，不上传图片
+                this.uploadData(null)
+            }else {
+                // 下面的代码将创建一个空的FormData对象:
+                var formData = new FormData()
+                // 你可以使用FormData.append来添加键/值对到表单里面；
+                this.fileList.forEach((file) => {
+                    formData.append('file', file.raw)
+                })
+                var url = this.lostOrFind ? "/api/finds/addImg" : "/api/losts/addImg"
+                this.$axios
+                    .post(url, formData)
+                    //判断上传是否成功，成功返回一个img的地址然后请求后台添加其他数据
+                    .then(res=>{
+                        if (res.status == "200") {
+                            this.uploadData(res.data.data);
+                        }
+                    })
+            }
+        },
+        uploadData(imgsUrlList) {
+            let tag = "", image = "";
+            console.log("haha",imgsUrlList)
+            console.log("haha",imgsUrlList.length)
+
+            //将tag数组转换成字符串
+            for (let i = 0; i < this.goods.selectedType.length; i++) {
+                tag += this.goods.selectedType[i] + '&';
+            }
+            //判断imgs是否存在,也就是是否上传了图片,存在就将url数组转换成字符串
+            if (imgsUrlList.length > 0) {
+                for (let i = 0; i < imgsUrlList.length; i++) {
+                    image += imgsUrlList[i] + '&';
+                }
+            }
+
+            //给后端数据
+            if (!this.lostOrFind) {
+                this.$axios({
+                    method: "post",
+                    url: "/api/losts/add",
+                    data: {
+                        uid: this.$store.state.user.id,
+                        title: this.goods.title,
+                        description: this.goods.desc,
+                        lostArea: this.goods.area + '&' + this.$store.state.user.location,
+                        lostTime: this.goods.date,
+                        tag: tag,
+                        image: image
+                    }
+                }).then(res => {
+                    this.goods = {};
+                    this.$router.push({path: '/'})
+                    this.$message.success("恭喜您修改成功,祝您一切顺利")
+                }).catch(err => {
+                    this.$message.error("服务器错误，稍后再修改吧")
+                })
+            } else {
+                this.$axios({
+                    method: "post",
+                    url: "/api/finds/add",
+                    data: {
+                        uid: this.$store.state.user.id,
+                        title: this.goods.title,
+                        description: this.goods.desc,
+                        foundArea: this.goods.area + '&' + this.$store.state.user.location,
+                        foundTime: this.goods.date,
+                        tag: tag,
+                        image: image
+                    }
+                }).then(res => {
+                    this.goods = {};
+                    this.$router.push({path: '/'})
+                    this.$message.success("恭喜您修改成功,祝您一切顺利")
+                }).catch(err => {
+                    this.$message.error("服务器错误，稍后再修改吧")
+                })
+            }
+        },
+
+        handleRemove(file, fileList) {
+            this.fileList = fileList
+        },
+        handleChange(file, fileList) {
+            this.fileList = fileList
+        },
+        handlePreview(file) {
+            console.log(file);
+        },
     },
+
     mounted() {
         this.load();
     }
@@ -143,6 +303,13 @@ export default {
         justify-content:flex-end;
     }
     .btn1 {
+        border: none;
+        border-radius: 0;
+        border-top-right-radius: 4px;
+        position: relative;
+        top: 41px;
+    }
+    .btn2{
         border: none;
         border-radius: 0;
         border-top-right-radius: 4px;
